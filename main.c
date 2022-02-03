bf0b5cff8770e5b5324c32fc2953ac4646a1c220
Denis Bazhenov <dotsid@gmail.com>
2022-02-03 22:51:49 +1000

    Restarting simavr when gdb session finished

--- main.c
+++ main.c
@@ -14,21 +14,25 @@ int main(int argc, char **argv) {
   fprintf(stdout, "Firmware: %s, mcu=%s, freq=%d\n", file_name, firmware.mmcu,
           firmware.frequency);
 
-  avr_t *avr = avr_make_mcu_by_name("atmega328p");
-  if (avr == NULL) {
-    fprintf(stderr, "[ERROR] Unable to create AVR core\n");
-    exit(1);
-  }
-  avr_init(avr);
-  avr_load_firmware(avr, &firmware);
+  for (;;) {
+    avr_t *avr = avr_make_mcu_by_name("atmega328p");
+    if (avr == NULL) {
+      fprintf(stderr, "[ERROR] Unable to create AVR core\n");
+      exit(1);
+    }
+    printf("Running AVR core...\n");
+    avr_init(avr);
+    avr_load_firmware(avr, &firmware);
 
-  avr->gdb_port = 1234;
-  avr->state = cpu_Stopped;
-  avr_gdb_init(avr);
+    avr->gdb_port = 1234;
+    avr->state = cpu_Stopped;
+    avr_gdb_init(avr);
 
-  int status = cpu_Running;
-  while ((status != cpu_Crashed) && (status != cpu_Done)) {
-    status = avr_run(avr);
+    int status = cpu_Running;
+    while ((status != cpu_Crashed) && (status != cpu_Done)) {
+      status = avr_run(avr);
+    }
+    avr_deinit_gdb(avr);
   }
   return 0;
 }
